3e0e81e02cbf75ad64c468908b4748ffe8e373fc
Jefferson Melo <jefferson.mello8@hotmail.com>
2025-07-17 20:48:27 -0300

    add: aumentando a cobertura de testes e aplicando page objects

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,21 +1,57 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
+
+import { getLoginPage } from '../support/pages/LoginPage'
 
 
-test('deve logar com sucesso', async ({page}) => {
+test('deve logar com sucesso', async ({ page }) => {
+
+    const loginPage = getLoginPage(page)
 
     const user = {
         username: 'jefferson',
         password: 'pwd123'
     }
 
-    await page.goto('http://localhost:3000/login')
-
-    await page.locator('#username').fill(user.username)
-    await page.locator('#password').fill(user.password)
-
-    await page.locator('button[type="submit"]').click()
+    loginPage.open()
+    loginPage.submit(user.username, user.password)
 
     const title = page.locator('h1')
     await expect(title).toContainText('Olá, Jefferson Sousa!')
 
+})
+
+test('não deve logar com sucesso', async ({ page }) => {
+
+    const loginPage = getLoginPage(page)
+
+    const user = {
+        username: 'jefferson',
+        password: '123456'
+    }
+
+    loginPage.open()
+    loginPage.submit(user.username, user.password)
+
+    const toast = page.locator('.toast')
+
+    await expect(toast).toContainText('Oops!')
+    await expect(toast).toContainText('Algo deu errado com seu login. Por favor, verifique suas credenciais')
+})
+
+test('não deve logar com usuário não cadastrado', async ({ page }) => {
+
+    const loginPage = getLoginPage(page)
+
+    const user = {
+        username: 'romario',
+        password: 'pwd123'
+    }
+
+    loginPage.open()
+    loginPage.submit(user.username, user.password)
+
+    const toast = page.locator('.toast')
+
+    await expect(toast).toContainText('Oops!')
+    await expect(toast).toContainText('Algo deu errado com seu login. Por favor, verifique suas credenciais')
 })
